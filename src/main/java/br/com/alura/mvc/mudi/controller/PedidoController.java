d395b309ef61e7f07d3a0384e9184fdc4442be21
filipe <filipe.olivetti@poloar.com.br>
2020-12-03 18:01:37 -0300

    adicionando a validação de campos

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,40 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidorepository;
+	
+	@GetMapping("formulario")
+	//colocado a requisição só pra nao dar erro ao carregar a pagina, que está utilizando
+	//na criação dos campos.
+	public String formulario(RequisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(@Validated RequisicaoNovoPedido requisicao, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidorepository.save(pedido);		
+
+		return "pedido/formulario";
+	}
+}
