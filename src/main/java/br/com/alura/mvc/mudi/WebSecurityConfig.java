4fb5467dec7b94deb8e7aef40eac68b2f48bed3f
filipe <filipe.olivetti@poloar.com.br>
2020-12-17 18:06:06 -0300

    refresh

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package br.com.alura.mvc.mudi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 
@@ -13,7 +18,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http
+		.authorizeRequests()
+			//aqui diz que o /home é publico ,não precisa se autenticar
+			//.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin(form -> form.loginPage("/login").permitAll());
+		
+	}
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("filipe")
+				.password("filipe")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 	
 }
